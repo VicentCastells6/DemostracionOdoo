491962050735ef8de25b819bf1b47365dcea58b9
VicentCastell6 <vicentecastellsdonat@gmail.com>
2025-03-18 10:48:34 +0000

    Rename

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Prueba',
+    'name': 'DemostracionOdoo',
     'version': '1.0',
-    'summary': 'Módulo de prueba',
-    'description': 'Este es un módulo de prueba para Odoo',
-    'author': 'Tu Nombre',
-    'website': "https://www.yourcompany.com",
+    'summary': 'Módulo de demo',
+    'description': 'Modulo de demo y pruebas de practicas de Odoo',
+    'author': 'Vicent Castells',
+    'website': "https://github.com/VicentCastells6",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Customizations',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -26,5 +26,10 @@
     ],
     'installable': True,
     'application': True,
+    
+    
+    
+    # Icon
+    'icon': '/Demostracion/static/description/icon.png',
 }
 
--- models/models.py
+++ models/models.py
@@ -3,15 +3,16 @@
 from odoo import models, fields, api
 
 
-class Prueba(models.Model):
-    _name = 'prueba.prueba'
-    _description = 'prueba.prueba'
+class Demo(models.Model):
+    _name = 'demo.demo'
+    _description = 'demo.demo'
 
     name = fields.Char(string="Name")
     value = fields.Integer(string="Value")
     value2 = fields.Float(compute="_value_pc", store=True, string="Value Percentage")
     description = fields.Text(string="Description")
     active = fields.Boolean(string="Active", default=True)
+    image = fields.Image('Image', max_width=128, max_height=128)
 
     @api.depends('value')
     def _value_pc(self):
